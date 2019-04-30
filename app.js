5a0570bd88e6b0bd2edbdabe10338bba96744ec0
admin <wangzezhong@huodongxing.com>
2019-04-30 19:12:10 +0800

    no message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('views', path.join(__dirname, './views'));
 app.get('/', function (req, res) {
     res.render('pug-html/index');
 });
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.listen(8070, () => {
     console.log('start  8070 port');
 });
